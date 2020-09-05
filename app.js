5bb7a55edbe0107de60028df1306535b1454ba49
p-r-i-y-a-n-k-a-533 <67096990+p-r-i-y-a-n-k-a-533@users.noreply.github.com>
2020-09-05 14:17:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,25 +16,25 @@ function checkplz(maau){
  switch (maau)
   {
     case "w":
-    {   var aud = new Audio("sounds/tom-1.mp3");
+    {   var aud = new Audio("./sounds/tom-1.mp3");
         aud.play(); } break;
     case "a":
-    {   var aud = new Audio("sounds/tom-2.mp3");
+    {   var aud = new Audio("./sounds/tom-2.mp3");
         aud.play(); } break;
     case "s":
-    {   var aud = new Audio("sounds/tom-3.mp3");
+    {   var aud = new Audio("./sounds/tom-3.mp3");
         aud.play(); } break;
     case "d":
-    {   var aud = new Audio("sounds/tom-4.mp3");
+    {   var aud = new Audio("./sounds/tom-4.mp3");
         aud.play(); } break;
     case "j":
-    {   var aud = new Audio("sounds/snare.mp3");
+    {   var aud = new Audio("./sounds/snare.mp3");
         aud.play(); } break;
     case "k":
-    {   var aud = new Audio("sounds/crash.mp3");
+    {   var aud = new Audio("./sounds/crash.mp3");
         aud.play(); } break;
     case "l":
-    {   var aud = new Audio("sounds/kick-bass.mp3");
+    {   var aud = new Audio("./sounds/kick-bass.mp3");
         aud.play(); } break;
   }
 }
